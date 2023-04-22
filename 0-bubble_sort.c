cecc34e55570ca34f58cd858bacfed4dfb11093f
WINNER2028 <obiwinner94@gmail.com>
2023-04-22 02:18:31 -0700

    awesomed

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -10,27 +10,27 @@
  */
 void bubble_sort(int *array, size_t size)
 {
-size_t i, m, new_m;
+size_t i, n, new_n;
 int swap;
 
 if (array == NULL || size < 2)
 return;
 
-m = size;
-while (m > 0)
+n = size;
+while (n > 0)
 {
-new_m = 0;
-for (i = 0; i < m - 1; i++)
+new_n = 0;
+for (i = 0; i < n - 1; i++)
 {
 if (array[i] > array[i + 1])
 {
 swap = array[i];
 array[i] = array[i + 1];
 array[i + 1] = swap;
-new_m = i + 1;
+new_n = i + 1;
 print_array(array, size);
 }
 }
-m = new_m;
+n = new_n;
 }
 }
